2a70be61d192736947196e844b5caf4c78e848a4
Giedrius <giedrius@blokas.io>
2024-11-14 08:38:53 +0000

    Add alsa_midi dependency to pyproject.toml

--- setup.py
+++ setup.py
@@ -18,8 +18,4 @@ from setuptools import find_packages, setup
 setup(
 	name='pimidipy',
 	packages=find_packages(include=['pimidipy']),
-	version='0.1.0',
-	description='pimidipy MIDI processing library',
-	author='Blokas',
-	requires='alsa_midi'
 )
