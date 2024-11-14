9f94a81807854170127ab6fdc62ba31598b66cc6
Giedrius <giedrius@blokas.io>
2024-11-14 08:06:47 +0000

    Adding pyproject.toml and copyright info

--- pimidipy/pimidipy.py
+++ pimidipy/pimidipy.py
@@ -1,3 +1,18 @@
+# pimidipy
+# Copyright (C) 2024  UAB Vilniaus blokas
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see https://www.gnu.org/licenses/.
+
 from collections import defaultdict
 from ctypes import Array
 from functools import partial
--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
+# pimidipy
+# Copyright (C) 2024  UAB Vilniaus blokas
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see https://www.gnu.org/licenses/.
+
 from setuptools import find_packages, setup
 
 setup(
@@ -6,4 +21,5 @@ setup(
 	version='0.1.0',
 	description='pimidipy MIDI processing library',
 	author='Blokas',
+	requires='alsa_midi'
 )
